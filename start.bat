27baabe0f1b565ba92a66747247729d78442f3a6
John Doe <johndoe@example.com>
2024-11-29 12:32:16 +0300

    buga fixa ZOV

--- start.bat
+++ start.bat
@@ -1,8 +1,9 @@
 @echo off
-set "pythonpath=.\.venv\bin\python.exe" # сюда путь для питона в венве
+set "pythonpath=python.exe" # сюда путь для питона в венве
 
 :loop
 %pythonpath% bot.py
+del ".\presentations\*.pdf"
 goto :loop
 
 pause
